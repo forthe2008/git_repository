e183b34839965afb4305361e09dc302081d48610
forthe2008 <1017241807@qq.com>
2017-01-14 17:00:31 +0800

    modify

--- sct_run.py
+++ sct_run.py
@@ -2,31 +2,103 @@
 import struct
 import socket
 import sys
-from xml.parsers.expat import ParserCreate
+import xml.dom.minidom
+
+length = {"u32" : 4, "u16" : 2, "u8" : 1}
 
 
+
+class xmlHandler(xml.sax.ContentHandler):
+    def start_element(name, attr):
+        print "start: %s, attr :%s" % (name, attr)
+
+    def end_element(name):
+        print "end: %s" % name
+
+    def data_element(data):
+        print "data: %s" % data
 
 class sct(object):
     
     def __init__(self, ip_addr, xml_file):
         self.ip_addr = ip_addr
         self.xml_file = xml_file
+        self.sct_list = []
         return
 
     def Run(self):
         #parse xml
         self.parseXml()
-        #tcp connect and send
+        #tcp struct data
+        self.structData()
+
+
+
         pass
 
     def parseXml(self):
+        docTree = xml.dom.minidom.parse(self.xml_file)
+        collection = docTree.documentElement
         
-        pass
+        #Get Testcase info
+        infos = collection.getElementsByTagName("INFO")
+        name = infos[0].getElementsByTagName("NAME")
+        brief = infos[0].getElementsByTagName("BRIEF")
+        print "Test Start:" + name[0].firstChild.data + "("+ brief[0].firstChild.data + ")"
+        
+        #Get Testcase
+        cases = collection.getElementsByTagName("CASE")
+        for case in cases:
+            #Get info
+            info = case.getElementsByTagName("INFO")[0]
+            uiname = info.getElementsByTagName("UINAME")[0]
+            brief = info.getElementsByTagName("BRIEF")[0]
+            
+            #Get testman
+            testman = collection.getElementsByTagName("TESTMAN")[0]
+            casename = testman.getElementsByTagName("CASENAME")[0]
+            caseid = testman.getElementsByTagName("CASEID")[0]
+
+            #Get message
+            message = collection.getElementsByTagName("MESSAGE")[0]
+            tbtsheader = message.getElementsByTagName("TBTSHEADER")[0]
+            subsystemname = tbtsheader.getElementsByTagName("SUBSYSTEMNAME")[0]
+            testcasename = tbtsheader.getElementsByTagName("TESTCASENAME")[0]    
+
+            #for params
+            params = message.getElementsByTagName("PARAM")
+            paramLength = 0
+            for param in params:
+                name = param.getAttribute("name")
+                valueType = param.getAttribute("valueType")
+                data = param.firstChild.data
+                
+                print name, length[valueType], data
+                paramLength += length[valueType]
+
+                
+
+
+            self.sct_list.append({"uiname":uiname.firstChild.data, 
+                                  "brief" : brief.firstChild.data, 
+                                  "casename" : casename.firstChild.data, 
+                                  "caseid" : caseid.firstChild.data, 
+                                  "subsystemname" : subsystemname.firstChild.data, 
+                                  "testcasename" : testcasename.firstChild.data,
+                                  "paramLength": paramLength})
+
+
+        return
 
     def tcpConnect(self):
-        pass
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(self.ip_addr, 0x315)
+        s.send()
+        s.close()
+        return
 
-    def structData(self):
+    def structData(self, msg_size, subsystemName, testCaseName, parametersSize, parameters):
+        buff = struct.pack(">HHIIHH48s48sI", 0, 0xFF00, 0x0F0104b8, 0,  msg_size, 0, subsystemName, testCaseName, parametersSize, ) 
         pass
 
 
